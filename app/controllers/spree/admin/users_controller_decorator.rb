937564f63df4573e18946b67242a278766dcb1f3
kinduff <abarcadabra@gmail.com>
2015-04-30 16:06:04 -0500

    Refactors admin user addresses, uses radio buttons, hook up actions

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -2,9 +2,6 @@ Spree::Admin::UsersController.class_eval do
   before_filter :build_user_addresses_hash, only: [:addresses, :edit_address]
 
   def addresses
-    # => Original action is below
-    # => Alias method is now edit_address
-    #
     # if request.put?
     #   if @user.update_attributes(user_params)
     #     flash.now[:success] = Spree.t(:account_updated)
@@ -19,17 +16,19 @@ Spree::Admin::UsersController.class_eval do
     @address = @user.addresses.find(params[:address_id])
     redirect_to admin_users_addresses_path unless @address
     if request.put?
-      @user.save_default_addresses(
-        params[:address_default_bill],
-        params[:address_default_ship],
-        @address
-      )
       if @user.update_attributes(user_params)
         flash.now[:success] = Spree.t(:account_updated)
       end
 
       redirect_to addresses_admin_user_path(@user)
     end
+  end
+
+  def update_addresses
+    @user = model_class.find(params[:user_id])
+    @user.update_attributes(params[:user].permit(:bill_address_id, :ship_address_id))
+    flash[:success] = Spree.t(:default_addresses_updated)
+    redirect_to addresses_admin_user_path(@user)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Spree::Core::Engine.add_routes do
     resources :users do
       get 'addresses/:address_id/edit', to: 'users#edit_address', as: :edit_address
       put 'addresses/:address_id/edit', to: 'users#edit_address', as: :edit_address_put
+      put 'addresses/update_addresses', to: 'users#update_addresses', as: :update_addresses
     end
   end
 
