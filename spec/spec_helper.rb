22253dd626faf50b6e07b954c2a02b6284498841
Mike Bourgeous <mike-bourgeous@users.noreply.github.com>
2015-07-28 18:33:58 -0600

    Load factories.  For some reason they did not load automatically.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,7 @@ require 'shoulda-matchers'
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[File.join(File.dirname(__FILE__), "support/**/*.rb")].each {|f| require f }
+Dir[File.join(File.dirname(__FILE__), "factories/**/*.rb")].each {|f| require f }
 
 
 # Requires factories defined in spree_core
