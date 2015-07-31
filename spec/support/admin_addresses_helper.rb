55d5c1da4aa33833349fca5cad7658cdbb3bdff5
Mike Bourgeous <mike-bourgeous@users.noreply.github.com>
2015-07-30 22:41:44 -0600

    Verify the text of every address is present on the address list page.

--- spec/support/admin_addresses_helper.rb
+++ spec/support/admin_addresses_helper.rb
@@ -5,7 +5,7 @@ module AdminAddresses
     visit spree.admin_addresses_path(user_id: user.id)
     expect(page).to have_content(I18n.t(:new_address, scope: :address_book))
 
-    expect_user_selection(user)
+    expect_user_addresses(user)
   end
 
   # Visits the address listing page for the given order and performs basic
@@ -14,8 +14,8 @@ module AdminAddresses
     visit spree.admin_addresses_path(order_id: order.id)
     expect(page).to have_content(I18n.t(:new_address, scope: :address_book))
 
-    expect_order_selection(order)
-    expect_user_selection(order.user) if order.user
+    expect_order_addresses(order)
+    expect_user_addresses(order.user) if order.user
   end
 
   # Expects +count+ addresses on the address listing page.
@@ -27,16 +27,25 @@ module AdminAddresses
     end
   end
 
-  # Expects the +user+ addresses to be selected on the page.
-  def expect_user_selection(user)
+  # Expects the +user+ addresses to be selected on the page, and expects every
+  # user address to be present on the page.
+  def expect_user_addresses(user)
     expect_selected(user.bill_address, :user, :bill)
     expect_selected(user.ship_address, :user, :ship)
+
+    user.addresses.each do |a|
+      expect(page).to have_content(content_regex(a))
+    end
   end
 
-  # Expects the +order+ addresses to be selected on the page.
-  def expect_order_selection(order)
+  # Expects the +order+ addresses to be selected on the page, and expects every
+  # order address to be present on the page.
+  def expect_order_addresses(order)
     expect_selected(order.bill_address, :order, :bill)
     expect_selected(order.ship_address, :order, :ship)
+
+    expect(page).to have_content(content_regex(order.bill_address)) if order.bill_address
+    expect(page).to have_content(content_regex(order.ship_address)) if order.ship_address
   end
 
   # Expects the +address+ to be selected on the page for the user or order
@@ -72,6 +81,13 @@ module AdminAddresses
   # Clicks on the address page's submit button.
   def submit_addresses
     click_button I18n.t(:update_default_addresses, scope: :address_book)
+  end
+
+  # Returns a Regexp that will find the address's text, ignoring case.
+  def content_regex(address)
+    # Regexp.escape adds backslashes before spaces, so the second gsub was
+    # necessary to get whitespace-insensitive matching.
+    Regexp.new(Regexp.escape(address.to_s.gsub(/<[^>]+>/, ' ')).gsub(/(\\?\s+)+/, '\s+'), 'i')
   end
 end
 
