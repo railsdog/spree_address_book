54fdbdad00a3ecb7c9c5a52a1adfbe9ea04e9b85
Mike Bourgeous <mike-bourgeous@users.noreply.github.com>
2015-08-18 19:56:11 -0600

    Moved wait_longer_for_ajax into spec_helper.rb for global use.
    It is called from spec_helper.rb.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -107,6 +107,16 @@ RSpec.configure do |config|
     Spree::Config[:company] = true # Test with the company field visible
   end
 
+  # TODO: Use Capybara's built-in AJAX waiting
+  def wait_longer_for_ajax
+    counter = 0
+    while page.evaluate_script("typeof($) === 'undefined' || $.active > 0")
+      counter += 1
+      sleep(0.1)
+      raise "AJAX request took longer than 20 seconds." if counter >= 200
+    end
+  end
+
   config.after(:each) do
     # Ensure js requests finish processing before advancing to the next test
     wait_longer_for_ajax if RSpec.current_example.metadata[:js]
--- spec/support/support_helper.rb
+++ spec/support/support_helper.rb
@@ -2,15 +2,6 @@ shared_context "support helper" do
 
   private
 
-  def wait_longer_for_ajax
-    counter = 0
-    while page.evaluate_script("typeof($) === 'undefined' || $.active > 0")
-      counter += 1
-      sleep(0.1)
-      raise "AJAX request took longer than 5 seconds." if counter >= 200
-    end
-  end
-
   # Disables JS confirmation popups in feature tests.
   def bypass_js_confirm
     page.evaluate_script('window.confirm = function() { return true; }')
