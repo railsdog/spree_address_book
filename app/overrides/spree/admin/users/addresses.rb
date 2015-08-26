5ef06494289608fb6615036e39197d2e59f8b7b5
Mike Bourgeous <mike@mikebourgeous.com>
2015-08-26 15:57:18 -0600

    Removed an unused and nonfunctional Deface override.

--- app/overrides/spree/admin/users/addresses.rb
+++
@@ -1,6 +0,0 @@
-Deface::Override.new(
-  :virtual_path => "spree/admin/users/addresses",
-  :name => "admin_user_addresses",
-  :replace => "[data-hook='admin_user_addresses']",
-  :partial => "spree/admin/users/address_book"
-)
