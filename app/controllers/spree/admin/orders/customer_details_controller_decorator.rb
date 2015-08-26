0d7b494c0ad852663f58d0404edd282308a6fdd2
Mike Bourgeous <mike@mikebourgeous.com>
2015-08-26 14:26:04 -0600

    Added a decorator to load deduplicated addresses on customer details.

---
+++ app/controllers/spree/admin/orders/customer_details_controller_decorator.rb
@@ -0,0 +1,23 @@
+# Adds @user and @addresses to the customer details controller so the address
+# list can be embedded on the order's customer details page.
+Spree::Admin::Orders::CustomerDetailsController.class_eval do
+  before_filter :set_user
+  before_filter :load_address_list
+
+  private
+  def set_user
+    if params[:user_id] && params[:user_id].to_i != @order.user_id
+      @user = Spree::User.find(params[:user_id])
+    end
+
+    @user ||= @order.user
+
+    if @order.user != @user
+      raise "Given user and order's user do not match!"
+    end
+  end
+
+  def load_address_list
+    @addresses = get_address_list(@order, @user)
+  end
+end
