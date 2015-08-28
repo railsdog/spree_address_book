0336cad756205ba62c2445b3cbf37f026945639f
Mike Bourgeous <mike@mikebourgeous.com>
2015-08-27 22:34:44 -0600

    Added billing address fields to the admin user creation form.

--- app/controllers/spree/admin/users_controller_decorator.rb
+++ app/controllers/spree/admin/users_controller_decorator.rb
@@ -5,6 +5,26 @@ Spree::Admin::UsersController.class_eval do
 
   before_filter :load_address_list
 
+  # UPDATE WITH SPREE (modified to change render :edit to redirect_to)
+  def create
+    if params[:user]
+      roles = params[:user].delete("spree_role_ids")
+    end
+
+    @user = Spree.user_class.new(user_params)
+    if @user.save
+
+      if roles
+        @user.spree_roles = roles.reject(&:blank?).collect{|r| Spree::Role.find(r)}
+      end
+
+      flash[:success] = Spree.t(:successfully_created, resource: @user.class.model_name.human)
+      redirect_to edit_admin_user_path(@user)
+    else
+      render :new
+    end
+  end
+
   private
   def load_address_list
     @addresses = get_address_list(nil, @user)
