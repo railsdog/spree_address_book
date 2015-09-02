8bc0d91a010aaf8e53ea1316def82d08d4cc50ae
Mike Bourgeous <mike@mikebourgeous.com>
2015-09-02 17:10:42 -0600

    Fixed address radio button selection for missing or invalid addresses.

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -1,11 +1,15 @@
 Spree::CheckoutController.class_eval do
   helper Spree::AddressesHelper
+  helper_method :get_selected_addresses
 
   before_filter :get_address_list
-  before_filter :set_addresses, :only => :update
+  before_filter :set_address_params, :only => :update
+
 
   protected
 
+  # Sets or re-sets @@addresses to a deduplicated address list for the order
+  # and user.
   def get_address_list
     if spree_current_user
       @addresses = Spree::AddressBookList.new(spree_current_user, @order)
@@ -14,7 +18,57 @@ Spree::CheckoutController.class_eval do
     end
   end
 
-  def set_addresses
+  # Sets the billing and shipping address that should be highlighted on the
+  # checkout address form in @bill_address and @ship_address.  Gives first
+  # priority to an unsaved address on the order, then to a saved address
+  # attached to the order, then to the user's default address.  Returns the
+  # billing and shipping address.
+  #
+  # Although it's ugly, this gets called from within the view since it needs to
+  # happen after the order is updated but before the view is rendered.
+  #
+  # TODO: Figure out a better way, perhaps by moving logic into the order model
+  def get_selected_addresses
+    puts ' get_selected_addresses '.center(72, '!') # XXX
+
+    get_address_list # Update @addresses in case addresses were set by #update
+
+    if @order.bill_address.present? && @order.bill_address_id.nil?
+      puts "\nSELECTING UN-saved order bill" # XXX
+      @bill_address = @order.bill_address
+    elsif @addresses.order_bill
+      puts "\nSELECTING saved order bill #{@addresses.order_bill.id}" # XXX
+      @bill_address = @addresses.order_bill
+    elsif @addresses.user_bill
+      puts "\nSELECTING saved user bill #{@addresses.user_bill.try(:id).inspect}" # XXX
+      @bill_address = @addresses.user_bill
+    else
+      puts "\nSELECTING first bill" # XXX
+      @bill_address = @addresses.first
+    end
+
+    if @order.ship_address.present? && @order.ship_address_id.nil?
+      puts "SELECTING UN-saved order ship\n\n" # XXX
+      @ship_address = @order.ship_address
+    elsif @addresses.order_ship
+      puts "SELECTING saved order ship #{@addresses.order_ship.id}\n\n" # XXX
+      @ship_address = @addresses.order_bill
+    elsif @addresses.user_ship
+      puts "SELECTING saved user ship #{@addresses.user_ship.try(:id).inspect}\n\n" # XXX
+      @ship_address = @addresses.user_ship
+    else
+      puts "SELECTING first ship\n\n" # XXX
+      @ship_address = @addresses.first
+    end
+
+    puts '!' * 72 # XXX
+
+    return @bill_address, @ship_address
+  end
+
+  # Changes address parameters to prevent duplicate addresses.
+  def set_address_params
+    whereami("SCC:sa:#{params}")
     return unless params[:order] && params[:state] == "address"
 
     check_address(:ship)
@@ -22,7 +76,9 @@ Spree::CheckoutController.class_eval do
   end
 
   # Retrieves and checks an address by ID or by attributes for the given +type+
-  # (:ship or :bill).
+  # (:ship or :bill), then updates params accordingly.
+  #
+  # TODO: This would probably be better off in a Spree::Order decorator
   def check_address(type)
     if type == :ship
       id_name = :ship_address_id
